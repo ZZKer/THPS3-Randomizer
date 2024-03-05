5f3eb0b2c65e3b3a2e8ffd2870cb90c1dbf2d711
ZZKer <Kyle.Mumenthaler@gmail.com>
2024-03-05 12:11:27 -0500

    What I have so far

---
+++ world/THPS3/__init__.py
@@ -0,0 +1,52 @@
+# world/THPS3/__init__.py
+
+import settings
+import typing
+from .options import MyGameOptions  # the options we defined earlier
+from .items import mygame_items  # data used below to add items to the World
+from .locations import mygame_locations  # same as above
+from worlds.AutoWorld import World
+from BaseClasses import Region, Location, Entrance, Item, RegionType, ItemClassification
+
+
+class MyGameItem(Item):  # or from Items import MyGameItem
+    game = "THPS3"  # name of the game/world this item is from
+
+
+class MyGameLocation(Location):  # or from Locations import MyGameLocation
+    game = "THPS3"  # name of the game/world this location is in
+
+
+class MyGameSettings(settings.Group):
+    class RomFile(settings.SNESRomPath):
+        """Insert help text for host.yaml here."""
+
+    rom_file: RomFile = RomFile("MyGame.sfc")
+
+
+class MyGameWorld(World):
+    """Tony Hawk's Pro Skater 3 is a skateboarding game for the PSx."""
+    game = "THPS3"  # name of the game/world
+    options_dataclass = MyGameOptions  # options the player can set
+    options: MyGameOptions  # typing hints for option results
+    settings: typing.ClassVar[MyGameSettings]  # will be automatically assigned from type hint
+    topology_present = True  # show path to required location checks in spoiler
+
+    # ID of first item and location, could be hard-coded but code may be easier
+    # to read with this as a property.
+    base_id = 1234
+    # instead of dynamic numbering, IDs could be part of data
+
+    # The following two dicts are required for the generation to know which
+    # items exist. They could be generated from json or something else. They can
+    # include events, but don't have to since events will be placed manually.
+    item_name_to_id = {name: id for
+                       id, name in enumerate(mygame_items, base_id)}
+    location_name_to_id = {name: id for
+                           id, name in enumerate(mygame_locations, base_id)}
+
+    # Items can be grouped using their names to allow easy checking if any item
+    # from that group has been collected. Group names can also be used for !hint
+    item_name_groups = {
+        "weapons": {"sword", "lance"},
+    }
---
+++ world/THPS3/locations.py
@@ -0,0 +1,24 @@
+# locations.py
+
+location_descriptions = {
+    "Foundry High Score": "High Score of 10,000 points in Foundry",
+    "Foundry Pro Score": "Pro Score of 40,000 points in Foundry",
+    "Foundry Sick Score": "Sick Score of 100,000 points in Foundry",
+	"Foundry SKATE": "Collect the SKATE letters in Foundry",
+	"Foundry Valves": "Unjam the 5 valves in Foundry",
+	"Foundry Press": "Activate the press in Foundry",
+	"Foundry Cannonball": "Cannonball over the halfpipe in Foundry",
+	"Foundry Control Booth": "Grind the control booth at the back of Foundry",
+	"Foundry Secret Tape": "Find the secret tape high up in Foundry",
+	"Foundry 100 Percent": "All goals, stats, and deck in Foundry",
+	"Foundry SP Press": "Stat Point above the press in Foundry",
+	"Foundry SP Catwalk": "Stat Point on the left side of the catwalk in Foundry",
+	"Foundry SP Left Balcony": "Stat Point above the control booth on the left side of the balcony in Foundry",
+	"Foundry SP Right Pipe": "Stat Point on right pipe near start in Foundry",
+	"Foundry SP Back Door": "Stat Point by the back door behind the control booth in Foundry",
+	"Foundry Deck": "Deck found at center of catwalks in Foundry",
+    "Foundry":
+        """
+        The group of all items in Foundry, the first level.
+		"""
+}
